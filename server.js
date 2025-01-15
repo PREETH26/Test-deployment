296f1a056de5bdb72a5298dc9c686d2e9129accf
Preetham Praveen <preethampraveencode26@gmail.com>
2025-01-15 11:59:58 +0530

    done server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get("/",(req,res)=>{
+    res.send("Hello, Kalvians! Your backend server is live!")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
